4799b104d1e8f7b4e1ea043350f01964138c1b1a
Jaimin Patel <jaimin@vertextechnology.com>
2020-06-07 22:12:39 +0530

    heroku deployment files are added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { customRegex, regex } = require('./regex');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000 ;
 
 // Enable CORS
 // app.use(cors());
